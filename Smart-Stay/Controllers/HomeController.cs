477b8641ec52b3e778c7b2996237d7f23ddbf6d4
rotondwa920-lab <rotondwa920@gmail.com>
2026-07-09 18:41:43 +0200

    Sign and Login

--- Smart-Stay/Controllers/HomeController.cs
+++ Smart-Stay/Controllers/HomeController.cs
@@ -18,10 +18,7 @@ namespace Smart_Stay.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
