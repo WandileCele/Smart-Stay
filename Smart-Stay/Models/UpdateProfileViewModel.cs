f7c0d663af69a3ee19046e5cc8f589f65bc6ef6a
g18c4646 <wandilecele28@gmail.com>
2026-09-04 15:38:58 +0200

    manage user accounts

---
+++ Smart-Stay/Models/UpdateProfileViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Smart_Stay.Models
+{
+    public class UpdateProfileViewModel
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Surname")]
+        public string SurName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        [RegularExpression(@"^0\d{9}$", ErrorMessage = "Phone number must start with 0 and contain exactly 10 digits.")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNo { get; set; } = null!;
+
+        [Display(Name = "New Password")]
+        [StringLength(15, MinimumLength = 4)]
+        public string? NewPassword { get; set; }
+    }
+}
